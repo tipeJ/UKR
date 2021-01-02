cee5869b4d212f62eafcfb9be6e3726701bc3840
tipej <tiitus.jarvinen@gmail.com>
2021-01-02 14:33:30 +0200

    Added padding to add_player_dialog

--- lib/ui/screens/add_player_dialog.dart
+++ lib/ui/screens/add_player_dialog.dart
@@ -47,78 +47,84 @@ class _AddPlayerDialogState extends State<AddPlayerDialog> {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Form(
-            key: _formKey,
-            child: Column(children: [
-              TextFormField(
-                validator: (value) =>
-                    value!.isEmpty ? "Please enter a name" : null,
-                controller: _nameController,
-                decoration: const InputDecoration(
-                    hintText: "Name of the server", labelText: "Name"),
-              ),
-              TextFormField(
-                validator: (value) =>
-                    value!.isEmpty ? "Please enter an address" : null,
-                controller: _addressController,
-                decoration: const InputDecoration(
-                    hintText: "Address of the server i.e. 192.168.xxx.xxx",
-                    labelText: "Address"),
-              ),
-              TextFormField(
-                validator: (value) =>
-                    value!.isEmpty ? "Plase enter a valid port number" : null,
-                controller: _portController,
-                decoration: const InputDecoration(
-                  hintText: "Port of the Kodi instance",
-                  labelText: "Port",
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+              key: _formKey,
+              child: Column(children: [
+                TextFormField(
+                  validator: (value) =>
+                      value!.isEmpty ? "Please enter a name" : null,
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                      hintText: "Name of the server", labelText: "Name"),
                 ),
-                keyboardType: TextInputType.number,
-                maxLength: 5,
-              ),
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  labelText: "Username (Optional)",
+                TextFormField(
+                  validator: (value) =>
+                      value!.isEmpty ? "Please enter an address" : null,
+                  controller: _addressController,
+                  decoration: const InputDecoration(
+                      hintText: "Address of the server i.e. 192.168.xxx.xxx",
+                      labelText: "Address"),
                 ),
-              ),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: "Password (Optional)",
-                ),
-              ),
-              Row(
-                children: [
-                  FlatButton(
-                    child: const Text("Cancel"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
+                TextFormField(
+                  validator: (value) =>
+                      value!.isEmpty ? "Plase enter a valid port number" : null,
+                  controller: _portController,
+                  decoration: const InputDecoration(
+                    hintText: "Port of the Kodi instance",
+                    labelText: "Port",
                   ),
-                  ElevatedButton(
-                    child: const Text("OK"),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        final newPlayer = Player(
-                          id: widget.initialValue?.id ?? uuid.v1(),
-                          name: _nameController.text,
-                          address: _addressController.text,
-                          port: int.parse(_portController.text),
-                          username: _credentialsGiven
-                              ? _usernameController.text
-                              : null,
-                          password: _credentialsGiven
-                              ? _passwordController.text
-                              : null,
-                        );
-                        Navigator.of(context).pop(newPlayer);
-                      }
-                    },
+                  keyboardType: TextInputType.number,
+                  maxLength: 5,
+                ),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: "Username (Optional)",
                   ),
-                ],
-              )
-            ])));
+                ),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: "Password (Optional)",
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      FlatButton(
+                        child: const Text("Cancel"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text("OK"),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            final newPlayer = Player(
+                              id: widget.initialValue?.id ?? uuid.v1(),
+                              name: _nameController.text,
+                              address: _addressController.text,
+                              port: int.parse(_portController.text),
+                              username: _credentialsGiven
+                                  ? _usernameController.text
+                                  : null,
+                              password: _credentialsGiven
+                                  ? _passwordController.text
+                                  : null,
+                            );
+                            Navigator.of(context).pop(newPlayer);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ])),
+        ));
   }
 }
