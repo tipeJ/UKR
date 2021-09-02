ee36a327a49711987b3809d5258a95758e47675a
tipej <tiitus.jarvinen@gmail.com>
2021-09-02 16:20:31 +0300

    Added cast item with thumbnail widget.

--- lib/ui/widgets/cast_item.dart
+++ lib/ui/widgets/cast_item.dart
@@ -10,13 +10,42 @@ class CastItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(name, style: Theme.of(context).textTheme.bodyText1),
-          Text(role, style: Theme.of(context).textTheme.caption),
-        ]
-      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(name, style: Theme.of(context).textTheme.bodyText1),
+        Text(role, style: Theme.of(context).textTheme.caption),
+      ]),
+    );
+  }
+}
+
+// Cast item with thumbnail image.
+class CastItemWithThumbnail extends StatelessWidget {
+  final String name;
+  final String role;
+  final String thumbnailUrl;
+
+  const CastItemWithThumbnail(
+      {required this.name, required this.role, required this.thumbnailUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3.0),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(4.0),
+          child: Image.network(thumbnailUrl,
+              width: 50.0, height: 50.0, fit: BoxFit.cover),
+        ),
+        SizedBox(width: 8.0),
+        Expanded(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(name, style: Theme.of(context).textTheme.bodyText1),
+            Text(role, style: Theme.of(context).textTheme.caption),
+          ]),
+        ),
+      ]),
     );
   }
 }
